abf95bb34dcdb05af6e614cf3209173265b1c2e1
billybarnie <haefguafe@gmail.com>
2023-07-15 15:52:16 -0500

    modifying my JS code

--- finals/scripts/finals.js
+++ finals/scripts/finals.js
@@ -4,44 +4,55 @@ document.querySelector('#year').textContent = currentDate;
 
 const pokeMons = document.getElementById('pokemonData');
 
-
 const fetchPokemon = async () => {
-    let pokemonList = [];
-    for (let i = 1; i <= 300; i++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      const abilities = data.abilities.map((ability) => ability.ability.name);
-      const stats = data.stats.find((stat) => stat.stat.name === 'attack');
-      const attackPower = stats ? stats.base_stat : 'N/A';
-
-      pokemonList.push({
-        name: data.name,
-        image: data.sprites['front_default'],
-        abilities: abilities.join(', '),
-        attackPower: attackPower,
-        id: data.id
-      });
-    }
+  const cachedData = localStorage.getItem('pokemonList');
+  if (cachedData) {
+    const pokemonList = JSON.parse(cachedData);
     getPokemon(pokemonList);
-};
-  
-const getPokemon = (pokemonList) => {
+  } else {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=300');
+    const data = await response.json();
 
+    const pokemonUrls = data.results.map((pokemon) => pokemon.url);
+    const pokemonList = await Promise.all(pokemonUrls.map(fetchPokemonData));
+
+    localStorage.setItem('pokemonList', JSON.stringify(pokemonList));
+
+    getPokemon(pokemonList);
+  }
+};
+
+const fetchPokemonData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const abilities = data.abilities.map((ability) => ability.ability.name);
+  const stats = data.stats.find((stat) => stat.stat.name === 'attack');
+  const attackPower = stats ? stats.base_stat : 'N/A';
+
+  return {
+    name: data.name,
+    image: data.sprites['front_default'],
+    abilities: abilities.join(', '),
+    attackPower: attackPower,
+    id: data.id,
+  };
+};
+
+const getPokemon = (pokemonList) => {
   const pokemonHTML = pokemonList
     .map(
       (pokemon) => `
       <li class="card" style="padding: 2%; margin: 2%; list-style-type: none;">
-        <img class="card-image" src="${pokemon.image}"/>
+        <img class="card-image" src="${pokemon.image}" loading="lazy"/>
         <h2 class="card-title">${pokemon.name}</h2>
         <p class="card-subtitle">Abilities: ${pokemon.abilities}</p>
         <p class="card-subtitle">Attack Power: ${pokemon.attackPower}</p>
       </li>
     `
-    ).join('');
+    )
+    .join('');
 
-    pokeMons.innerHTML = pokemonHTML;
-
+  pokeMons.innerHTML = pokemonHTML;
 };
 
 fetchPokemon();
